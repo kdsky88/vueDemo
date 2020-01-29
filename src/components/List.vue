ad79539a9882db25f62ec09a4c0833068dfdf1b7
kdsky88 <kdsky88@gmail.com>
2020-01-29 10:40:51 +0900

    add vuex

--- src/components/List.vue
+++ src/components/List.vue
@@ -27,50 +27,35 @@ ul li.checked::before {
 </style>
 <template>
 	<ul id="todolist">
-		<li v-for="a in todolist" :key="a.id" :class="checked(a.done)" @click="doneToggle(a.id)">
+		<li v-for="a in todolist" :key="a.id" :class="checked(a.done)" @click="doneToggle({id : a.id})">
 			<span>{{a.todo}}</span>
 			<span v-if="a.done"> (완료)</span>
-			<span class="close" v-on:click.stop="deleteTodo(a.id)">&#x00D7;</span>
+			<span class="close" v-on:click.stop="deleteTodo({id:a.id})">&#x00D7;</span>
 		</li>
 	</ul>
 </template>
 <script type="text/javascript">
-import eventBus from '../EventBus'
+import Constant from '../Constant'
+import { mapState, mapMutations} from 'vuex'
 
 export default {
-	name : 'list',
-	created : function() {
-		eventBus.$on('add-todo', this.addTodo);
-	},
-	data : function() {
-		return {
-			todolist : [
-				{ id:1, todo : "영화보기", done:false },
-				{ id:2, todo : "주말 산책", done:true },
-				{ id:3, todo : "ES6 학습", done:false },
-				{ id:4, todo : "잠실 야구장", done:false },
-			]
-		}
-	},
+	name : 'List',
+	computed : mapState(['todolist']),
 	methods : {
 		checked : function(done) {
 			if(done) return { checked:true};
 			else return {checked:false};
 		},
-		addTodo : function(todo) {
-			if (todo !== "") {
-				this.todolist.push(
-					{ id: new Date().getTime(), todo: todo, done:false});
-			}
+		deleteTodo  : function(payload) {
+			this.$store.dispatch(Constant.DELETE_TODO, payload);
 		},
-		doneToggle : function(id) {
-			var index = this.todolist.findIndex((item)=>item.id === id);
-			this.todolist[index].done = !this.todolist[index].done;
+		doneToggle : function(payload) {
+			this.$store.dispatch(Constant.DONE_TOGGLE, payload);
 		},
-		deleteTodo : function(id) {
-			var index = this.todolist.findIndex((item)=>item.id === id);
-			this.todolist.splice(index,1);
-		}
+		...mapMutations([
+			Constant.DELETE_TODO,
+			Constant.DONE_TOGGLE
+		])
 	}
 }
 </script>
